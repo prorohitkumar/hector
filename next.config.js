a2a44051525b84aadb4a90e706ad896f1e0b73f7
Rohit <rohit.1.kumar@stackroute.in>
2024-10-16 12:57:00 +0530

    init

--- next.config.js
+++ next.config.js
@@ -8,6 +8,11 @@ const nextConfig = {
       'hebbkx1anhila5yf.public.blob.vercel-storage.com',
     ],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
